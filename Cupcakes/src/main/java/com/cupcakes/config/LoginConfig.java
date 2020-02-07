356300e38288aa387dd96a854a12c68d36873e99
KAUSHIK SOWMYA <KAUSHIK SOWMYA@LAPTOP-MH5L6UM0>
2020-02-07 17:00:12 +0530

    Changed from xml based configuration to annotation based configuration

---
+++ Cupcakes/src/main/java/com/cupcakes/config/LoginConfig.java
@@ -0,0 +1,10 @@
+package com.cupcakes.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.cupcakes.controller"})
+public class LoginConfig {
+
+}
